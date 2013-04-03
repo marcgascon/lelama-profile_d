0e07959ce8994f35c0081b0788929904f8665488
Sander Bilo <sander.bilo@gmail.com>
2013-04-03 16:23:50 +0200

    Adding user profile.d changes

--- Modulefile
+++ Modulefile
@@ -1,11 +1,9 @@
-name    'klangrud-profile_d'
-version '0.0.1'
-source 'git://github.com/buckle/klangrud-profile_d.git'
+name 'proteon-profile_d'
+version '0.1.0'
+
 author 'klangrud'
 license 'Apache Version 2.0'
+project_page 'http://github.com/buckle/klangrud-profile_d.git'
+source 'git://github.com/buckle/klangrud-profile_d.git'
 summary 'profile.d Module'
 description 'profile.d Module for RHEL, CentOS, Debian, Ubuntu'
-project_page 'http://github.com/buckle/klangrud-profile_d.git'
-
-## Add dependencies, if any:
-# dependency 'username/name', '>= 1.2.0'
--- manifests/script.pp
+++ manifests/script.pp
@@ -1,38 +1,42 @@
-define profile_d::script($ensure = 'present', $content = '', $content_file = '', $source = '', $shell = '/bin/sh' ) {
+define profile_d::script ($ensure = 'present', $content = '', $content_file = '', $source = '', $shell = '/bin/sh', $user = undef) {
+    include profile_d
 
-  include profile_d
+    if ($content == '') and ($content_file == '') and ($source == '') {
+        fail('You must pass in one of the following parameters $content, $content_file, $source on definition')
+    }
 
-  if ($content == '') and ($content_file == '') and ($source == '') {
-    fail('You must pass in one of the following parameters $content, $content_file, $source on definition')
-  }
+    if (($content != '' and ($content_file != '' or $source != '')) or ($source != '' and $content_file != '')) {
+        fail('Only one of the following $content, $content_file, $source parameters can be set on definition')
+    }
 
-  if (($content != '' and ($content_file != '' or $source != '')) or ($source != '' and $content_file != '')) {
-    fail('Only one of the following $content, $content_file, $source parameters can be set on definition')
-  }
+    if ($content != '') {
+        $content_value = inline_template("<%= scope.function_template(['profile_d/header.erb']) -%>$content")
+    } elsif ($content_file != '') {
+        $content_value = template($content_file)
+    } else {
+        $content_value = undef
+    }
 
-  if ($content != '') {
-    $content_value = inline_template("<%= scope.function_template(['profile_d/header.erb']) -%>
-$content")
-  } elsif ($content_file != '') {
-    $content_value = template($content_file)
-  } else {
-    $content_value = undef
-  }
+    if ($source != '') {
+        $source_value = $source
+    } else {
+        $source_value = undef
+    }
 
-  if ($source != '') {
-    $source_value = $source
-  } else {
-    $source_value = undef
-  }
-
-  file {"/etc/profile.d/${name}":
-    ensure  => $ensure,
-    owner   => root,
-    group   => root,
-    mode    => '0644',
-    content => $content_value,
-    source  => $source_value,
-    require => File['/etc/profile.d'],
-  }
-
+    if ($user != undef) {
+        exec { "Adding ${name} for ${user}":
+            command => "sudo -u ${user} mkdir -p ~${user}/.profile.d && echo '${content_value}' > ~${user}/.profile.d/${name} ",
+            unless  => "cat ~${user}/.profile.d/${name} | grep '${content}'",
+        }
+    } else {
+        file { "/etc/profile.d/${name}":
+            ensure  => $ensure,
+            owner   => root,
+            group   => root,
+            mode    => '0644',
+            content => $content_value,
+            source  => $source_value,
+            require => File['/etc/profile.d'],
+        }
+    }
 }
