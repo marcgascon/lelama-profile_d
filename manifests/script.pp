c48ac8f9ac5292ea76f30bca47aba6ce3d2ea05d
Sander Bilo <sander.bilo@gmail.com>
2013-04-05 15:31:29 +0200

    Bugfixes
    - input sanitation for user profile_d scripts
    - ensure => absent also removes user scripts now
    - source parameter not valid in combination with the source parameter

--- manifests/script.pp
+++ manifests/script.pp
@@ -9,13 +9,17 @@ define profile_d::script (
     $user = undef
 ) {
     include profile_d
-
+    
     if ($content == '') and ($content_file == '') and ($source == '') {
         fail('You must pass in one of the following parameters $content, $content_file, $source on definition')
     }
 
     if (($content != '' and ($content_file != '' or $source != '')) or ($source != '' and $content_file != '')) {
         fail('Only one of the following $content, $content_file, $source parameters can be set on definition')
+    }
+    
+    if (($source != '' and $user != undef)) {
+        fail('It is not possible to use $source in combination with $user')
     }
 
     if ($content != '') {
@@ -33,9 +37,20 @@ define profile_d::script (
     }
 
     if ($user != undef) {
-        exec { "Adding ${name} for ${user}":
-            command => "sudo -u ${user} mkdir -p ~${user}/.profile.d && echo '${content_value}' > ~${user}/.profile.d/${name} ",
-            unless  => "cat ~${user}/.profile.d/${name} | grep '${content}'",
+        $content_value_safe = regsubst($content_value, "(')", "'\\\\''",'G') 
+
+        if ($ensure == present) {
+            exec { "Adding ${name} for ${user}":
+                command => "sudo -u ${user} mkdir -p ~${user}/.profile.d && echo '${content_value_safe}' > ~${user}/.profile.d/${name}",
+                unless  => "cat ~${user}/.profile.d/${name} | grep '${content}'",
+                require => User[$user],
+            }
+        } else {
+            exec { "Removing ${name} for ${user}":
+                command => "sudo -u ${user} rm ~${user}/.profile.d/${name}",
+                onlyif  => "test -e ~${user}/.profile.d/${name}",
+                require => User[$user],
+            }
         }
     } else {
         file { "/etc/profile.d/${name}":
